2779006d9e5cb09f08e6094e00cc63fb21c369f8
Rolando-PaloIT <enajera@palo-it.com>
2019-01-05 13:13:20 -0600

    use of process to read parameters

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 let mult = require('./multiply/multiply');
 
-let base = '5';
+let argv = process.argv;
+let parameter = argv[2];
+let base = parameter.split('=')[1];
 
 mult.createFile(base)
     .then(file => console.log(`file created ${ file }`))
